5102d962f5a6cce34476e448a339e41235ffce64
S Thirumurugan <hiz1cob@bosch.com>
2024-05-07 10:41:25 +0530

    Added ticks utilities and options

---
+++ options.h
@@ -0,0 +1,15 @@
+#ifndef SCRCPY_OPTIONS_H
+#define SCRCPY_OPTIONS_H
+
+#include "common.hpp"
+#include "util/ticks.h"
+
+enum sc_log_level {
+    SC_LOG_LEVEL_VERBOSE,
+    SC_LOG_LEVEL_DEBUG,
+    SC_LOG_LEVEL_INFO,
+    SC_LOG_LEVEL_WARN,
+    SC_LOG_LEVEL_ERROR,
+};
+
+#endif
