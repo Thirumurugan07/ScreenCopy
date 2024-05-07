12c1e3c2e70176dd587be0ea4b1bd449d713c7bb
FIXED-TERM Kapaganti Raghuram (MS/EHM-TS1) <paz1cob@bosch.com>
2024-05-07 14:40:37 +0530

    log header file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include "util/net.hpp"
+#include "util/binary.h"
+#include "util/ticks.h"
 using namespace std;
 
 int main() {
-	cout << "Hello, WOrld";
+	//cout << "Hello, WOrld";
 }
